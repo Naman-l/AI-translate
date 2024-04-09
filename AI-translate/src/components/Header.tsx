f3b2f8004271df0db9041df9c5739b3b38330deb
swapnilawasthi-dev <165567232+swapnilawasthi-dev@users.noreply.github.com>
2024-04-09 16:48:27 +0530

    Home Page + Navbar

--- AI-translate/src/components/Header.tsx
+++ AI-translate/src/components/Header.tsx
@@ -1,38 +1,130 @@
-import { Box, Typography, colors, css } from "@mui/material"
-import React from "react";
+import React, { useState } from "react";
+import { Box, Typography, useMediaQuery, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import {useTheme} from '@mui/material';
+import {ThemeProvider} from '@mui/styles';
+import { useNavigate } from "react-router-dom";
+import logo from '../assets/logo.svg';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-    // improve header css
-    // height choti karni hai
-    // better background colors
-    // add nav items(contacts us, home , tranlsate) top right, route
+  const buttons = [
+    {
+      title: "Home",
+      to: "/home",
+    },
+    {
+      title: "Transate",
+      to: "/translate",
+    },
+    {
+      title: "Contact Us",
+      to: "/contact",
+    },
+
+  ]
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setDrawerOpen(open);
+  };
 
   return (
     <Box
       sx={{
         bgcolor: "#fff",
-        height: "56px",
+        opacity: 0.9,
+        position: 'fixed',
+        zIndex: 1041,
+        boxShadow: 3,
+        top: '0px',
+        left: '0px',
+        right: '0px',
+        height: "50px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        padding: "10px 24px 10px 24px",
-        color:'#000',
-        width:'100%'
+        padding: "0 20px",
+        color: '#000',
+        width: '100%',
       }}
     >
       <Box
         sx={{
           display: "flex",
           alignItems: "center",
-          gap: 2.5,
           cursor: "pointer",
         }}
+        onClick={() => navigate("/home")}
       >
-        accounts header
+        <img src={logo} alt="logo" style={{ height:'30px', cursor: 'pointer' }} />
       </Box>
+      {isSmallScreen ? (
+        <>
+          <IconButton
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+            sx={{ padding: 0 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            sx={{
+              // '& .MuiDrawer-root': {
+              //     position: 'absolute',
+              //     top: '50px'
+              // },
+              '& .MuiPaper-root': {
+                  position: 'absolute',
+                  top: '52px'
+              },
+            }}
+            anchor="top"
+            open={drawerOpen}
+            onClose={toggleDrawer(false)}
+            BackdropProps={{ invisible: true }}
+          >
+            {buttons.map((button, index) => {
+              return (
+              <ListItem key={index} onClick={() => { navigate(button.to); setDrawerOpen(false); }}>
+                <ListItemText sx={{"&:hover": { color: "orange" }}} primary={button.title} />
+              </ListItem>
+            )})}
+            </Drawer>
+        </>
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          {buttons.map((button, index) => {
+            return (
+              <Typography
+                key={index}
+                variant="body1"
+                sx={{ padding: '5px',marginRight: '10px', cursor: 'pointer', "&:hover": { color: "orange" } }}
+                onClick={() => navigate(button.to)}
+              >
+                {button.title}
+              </Typography>
+          )})}
+        </Box>
+      )}
     </Box>
   );
 };
 
 export default Header;
+
--- AI-translate/src/pages/Translate.tsx
+++ AI-translate/src/pages/Translate.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../components/Header'
 
 export default function Translate() {
   return (
-    <div>
-        transalte
+    <Box>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <Header/>
       
-    </div>
+    </Box>
   )
 }
